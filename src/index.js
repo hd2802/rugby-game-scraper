1ca17875f7a86af4193e87a0e141af1866b2496d
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-11-13 15:04:54 +0000

    Revert "TO REVERT"
    This reverts commit 4ff0dfea759f85fec2a64a68d222d9afc64ede33.

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import { isFileEmpty } from "./utils/fileChecker.js"
-
-const main = async () => {
-    console.log("hello world")
-    console.log(isFileEmpty("./data/french_data.txt"))
-}
-
-main()
