d339be2bba33793f9444f0f06bac8666b02991fa
Madeleine Hartland-Dykes <mhartlanddykes18@gmail.com>
2025-11-18 14:00:51 +0000

    Adds starting outline for the scraper (refactored)

---
+++ page-scrapers/tourn.py
@@ -0,0 +1,10 @@
+""" 
+This code will read from tournament_links.txt, with each link, the code will find the link for each club in the league.
+Then each url of the club will be saved in a new file in out/club_links.json that stores the data as such:
+
+    [
+        {"Premiership": [
+            "https://northhamptonsaintsurl.com"
+        ]}
+    ]
+"""
